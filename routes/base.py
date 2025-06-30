2faac2fadd5c64f4793fcc000fdd1a8a8e121175
AliElOu <alielouankrimi@gmail.com>
2025-06-30 23:58:33 +0100

    config env variables

--- routes/base.py
+++ routes/base.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-
+import os
 base_router = APIRouter()
 
 
 @base_router.get("/")
 def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
     return {
-        "message": "Welcome!"
+        "message": f"Welcome to {app_name} v{app_version}!"
     }
